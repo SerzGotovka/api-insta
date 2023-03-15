d129595f964de20cb0df8488d90cd9e38e9f3b59
Сергей Готовка <112329009+SerzGotovka@users.noreply.github.com>
2023-03-15 19:33:57 +0300

    Add setializers on COMMENT

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('posts/', views.PostList.as_view(), name='api-posts'),
     path('post/<int:pk>', views.PostDetail.as_view(), name='api-post'),
     path('posts/user/<int:id>', views.PostsByUser.as_view(), name='api-user-posts'),
-
+    path('comments/', views.CommentList.as_view(), name='api-comments'),
+    path('comments/post/<int:id>/', views.CommentByPost.as_view(), name="api-comment-post"),
+    path('comments/user/<int:id>/', views.CommentByUser.as_view(), name="api-comment-user"),
+    path('comment/<int:pk>/', views.CommentDetail.as_view(), name='api-comment'),
 ]
--- api/views.py
+++ api/views.py
@@ -35,6 +35,34 @@ class PostsByUser(APIView):
         return Response(serializer.data)
 
 
+class CommentByPost(APIView):
+
+    def get(self, request, id):
+        comment_by_post = Comment.objects.filter(post__id=id)
+        serializer = serializers.CommentSerializer(comment_by_post, many=True)
+
+        return Response(serializer.data)
+
+
+class CommentByUser(APIView):
+
+    def get(self, request, id):
+        comment_by_user = Comment.objects.filter(user__id=id)
+        serializer = serializers.CommentSerializer(comment_by_user, many=True)
+
+        return Response(serializer.data)
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+
 
 
 
